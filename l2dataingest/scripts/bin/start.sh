5d87f74ac9828c105cc2e2fd7fb400b39297f273
Wojciech Pluta <60693402+WSPluta@users.noreply.github.com>
2023-06-06 11:13:07 +0100

    l2dataingestion clearup

--- l2dataingest/scripts/bin/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sudo systemctl start rabbitmq-server
-sudo systemctl start f1sim-producer
-sudo systemctl start f1sim-consumer
-# sudo systemctl start f1sim-webserver
--- l2dataingest/scripts/bin/stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sudo systemctl stop rabbitmq-server
-sudo systemctl stop f1sim-producer
-sudo systemctl stop f1sim-consumer
